339af3101aba81863372f72e35c76f4cdb2ac56a
Zion Nimchuk <zionnimchuk@gmail.com>
2016-09-30 00:59:46 -0700

    Initial work on fancy collision for tilmap

--- src/tilemaptest.nim
+++ src/tilemaptest.nim
@@ -21,15 +21,16 @@ let spriteSheetIntRects: exampleSSIntRect = [
   ]
 let level =
     [
-        0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11,
-        12, 13, 14, 15,
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1
+        16, 30, 31, 31, 32, 16, 16, 30, 31, 31,
+        16, 16, 16, 16, 16, 16, 16, 16, 16, 46,
+        16, 16, 16, 16, 16, 16, 16, 16, 16, 46,
+        2 , 16, 16, 16, 46, 16, 16, 16, 16, 46,
+        27, 28, 28, 2 , 16, 16, 16, 16, 16, 46,
+        13, 13, 13, 14, 16, 16, 16, 16, 16, 46,
+        13, 13, 13, 14, 16, 16, 16, 6 , 7 , 7 ,
+        25, 25, 25, 26, 16, 16, 16, 30, 31, 31,
+        16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+        0 , 1 , 1 , 1 , 2 , 16, 6 , 7 , 7 , 7 ,
     ]
 
 ##
@@ -38,101 +39,77 @@ let level =
 var m_vertices*: VertexArray
 var spriteSheet = newTexture("example-tileset.png")
 proc createTileMap[I](width, height: int, level: array[I, int], tileSize: Vect, scale: float = 1.0f, blankNumber: int = 0, usePhysics: bool = false): bool =
-  m_vertices = newVertexArray(PrimitiveType.Quads)
-  var count: int = 0
-  var drawHeight: int = 0
-  var drawWidth: int = 0
-  var spriteHeight: int = 0
-  for i in 0..width*height:
-    if width*height > level.len:
+  #Some basic error checking.
+  if width*height > level.len:
       echo "ERROR: Declaring a size that is larger than the level array length!"
-      break
+      return
+  #Declaring the Vertex array and specifying it's type
+  m_vertices = newVertexArray(PrimitiveType.Quads)
+  var 
+    drawHeight: int = 0
+    drawWidth: int = 0
+    physicsCount: int = 0
+    previousLine = newSeq[int](width)
+  let
+    xTileSize = tileSize.x * scale
+    yTileSize = tileSize.y * scale
+  for i in 0..width*height-1:
     if drawWidth >= width:
       drawHeight += 1
       drawWidth = 0
-      spriteHeight += 1
-      echo "yeah"
-    #if spriteSheet.size.y < drawHeight:
-    # spriteHeight += 1
-    #elif spriteHeight > spriteSheet.size.y:
-    #  spriteHeight = 0
-    var xTileSize = tileSize.x * scale
-    var yTileSize = tileSize.y * scale
-    var spriteWidth = level[i]
-    var spritePosX: int = 0
-    var spritePosY: int = 0
-    var texting: int = 0 
-    texting = ((level[i].toFloat * tileSize.x) / spriteSheet.size.x.toFloat).floor.toInt
-    var spriteWidthCurrent: float
-    #if (level[i].toFloat * tileSize.x).toInt >= (int)spriteSheet.size.y.toFloat:
-      
-    spritePosX = ((int)level[i].toFloat * tileSize.x) - spriteSheet.size.x * texting
-      
-      
-    #else:
-    #  echo "yas!"
-    #  spritePosX = ((int)level[i].toFloat * tileSize.x)
-      #spriteWidthCurrent = 
-      #texting = 1
-    spriteWidthCurrent = (texting).toFloat * tileSize.y
-    echo "texting variable is: ", texting
-    echo "level id: ", (level[i].toFloat * tileSize.x).toInt
-    echo "spriteWidthCurrent: ", spriteWidthCurrent
-    m_vertices.append vertex(vec2(drawWidth.toFloat * xTileSize, drawHeight.toFloat * yTileSize),                             White, vec2(spritePosX.toFloat, spriteWidthCurrent))
-    m_vertices.append vertex(vec2(drawWidth.toFloat * xTileSize + (xTileSize), drawHeight.toFloat * yTileSize),               White, vec2(spritePosX.toFloat + tileSize.x, spriteWidthCurrent))
-    m_vertices.append vertex(vec2(drawWidth.toFloat * xTileSize + (xTileSize), drawHeight.toFloat * yTileSize + (yTileSize)), White, vec2(spritePosX.toFloat + tileSize.x, spriteWidthCurrent + tileSize.y))
-    m_vertices.append vertex(vec2(drawWidth.toFloat * xTileSize, drawHeight.toFloat * yTileSize + (yTileSize)),               White, vec2(spritePosX.toFloat, spriteWidthCurrent + tileSize.y))
-    echo m_vertices[i].position
-    #m_vertices.append vertex(vec2((i + 1).toFloat * tileSize.x * scale, j.toFloat * tileSize.y * scale), White, vec2((tu + 1) * tileSize.x.toInt, tv * tileSize.y.toInt))
-    #m_vertices.append vertex(vec2((i + 1).toFloat * tileSize.x * scale, (j + 1).toFloat * tileSize.y * scale), White, vec2((tu + 1) * tileSize.x.toInt, (tv + 1) * tileSize.y.toInt))
-    #m_vertices.append vertex(vec2(i.toFloat * tileSize.x * scale, (j + 1).toFloat * tileSize.y * scale), White, vec2(tu * tileSize.x.toInt, (tv + 1) * tileSize.y.toInt))
-    drawWidth += 1
+      for p in 0..width-1:
+        previousLine[p] = level[(width*drawHeight)+p]
+      echo previousLine
 
-  for i in countup(0, width-1):
-    if true:
-      break
-    for j in countup(0, height-1):
+    let layer: int = ((level[i].toFloat * tileSize.x) / spriteSheet.size.x.toFloat).floor.toInt
       
-      #NOTE! Algorithm renders them from top to bottom
-      #var tileNumber: int = level[i + j * width]
-      #var tu: int = (tileNumber) mod (spriteSheet.size.x / tileSize.x.toInt).toInt
-      #var tv: int = (int)tileNumber / ((int)spriteSheet.size.x / (int)tileSize.x.toInt)
-      var newColor = White
-      if count == 0:
-        newColor = Red
-      elif count == 1:
-        newColor = Green
-      elif count == 2:
-        newColor = Blue
-      elif count == 3:
-        newColor = Black
-      elif count == 4:
-        newColor = Magenta
+    var spritePosX = (level[i].toFloat * tileSize.x) - spriteSheet.size.x.toFloat * layer.toFloat
+    
+    var spriteWidthCurrent: float = layer.toFloat * tileSize.y
+    
+    m_vertices.append vertex(
+      vec2(drawWidth.toFloat * xTileSize, drawHeight.toFloat * yTileSize), White, 
+      vec2(spritePosX, spriteWidthCurrent))
+    
+    m_vertices.append vertex(
+      vec2(drawWidth.toFloat * xTileSize + (xTileSize), drawHeight.toFloat * yTileSize), White, 
+      vec2(spritePosX + tileSize.x, spriteWidthCurrent))
+    
+    m_vertices.append vertex(
+      vec2(drawWidth.toFloat * xTileSize + (xTileSize), drawHeight.toFloat * yTileSize + (yTileSize)), White, 
+      vec2(spritePosX + tileSize.x, spriteWidthCurrent + tileSize.y))
+    
+    m_vertices.append vertex(
+      vec2(drawWidth.toFloat * xTileSize, drawHeight.toFloat * yTileSize + (yTileSize)), White, 
+      vec2(spritePosX, spriteWidthCurrent + tileSize.y))
+    drawWidth += 1
+    if level[i] == blankNumber:
+      #echo "test"
+      
+      for d in 0..2:
+        #echo m_vertices[physicsCount+d].position.x
+        #echo "physicsCount + d is: ", physicsCount+d
+        #echo d
+        var ground = newSegmentShape(space.staticBody, v(m_vertices[physicsCount+d].position.x, m_vertices[physicsCount+d].position.y), v(m_vertices[physicsCount+d+1].position.x, m_vertices[physicsCount+d+1].position.y), 0)
+        ground.friction = 20.0
+        #discard space.addShape(ground)
+      var ground = newSegmentShape(space.staticBody, v(m_vertices[physicsCount].position.x, m_vertices[physicsCount].position.y), v(m_vertices[physicsCount+3].position.x, m_vertices[physicsCount+3].position.y), 0)
+      ground.friction = 20.0
+      #discard space.addShape(ground)
+      #count += 1
+    else:
+      if drawHeight == 0:
+        var ground = newSegmentShape(space.staticBody, v(m_vertices[physicsCount].position.x, m_vertices[physicsCount].position.y), v(m_vertices[physicsCount+1].position.x, m_vertices[physicsCount+1].position.y), 0)
+        ground.friction = 20.0
+        discard space.addShape(ground)
       else:
-        newColor = Yellow
-      #m_vertices.append vertex(vec2(i.toFloat * tileSize.x * scale, j.toFloat * tileSize.y * scale), newColor, vec2(tu * tileSize.x.toInt, tv * tileSize.y.toInt))
-      #m_vertices.append vertex(vec2((i + 1).toFloat * tileSize.x * scale, j.toFloat * tileSize.y * scale), newColor, vec2((tu + 1) * tileSize.x.toInt, tv * tileSize.y.toInt))
-      #m_vertices.append vertex(vec2((i + 1).toFloat * tileSize.x * scale, (j + 1).toFloat * tileSize.y * scale), White, vec2((tu + 1) * tileSize.x.toInt, (tv + 1) * tileSize.y.toInt))
-      #m_vertices.append vertex(vec2(i.toFloat * tileSize.x * scale, (j + 1).toFloat * tileSize.y * scale), White, vec2(tu * tileSize.x.toInt, (tv + 1) * tileSize.y.toInt))
-      echo "######"
-      echo m_vertices[count].position
-      echo m_vertices[count+1].position
-      echo m_vertices[count+2].position
-      echo m_vertices[count+3].position
-      echo count
-      echo m_vertices[count].texCoords
-      echo m_vertices[count+1].texCoords
-      echo m_vertices[count+2].texCoords
-      echo m_vertices[count+3].texCoords
-      #echo level[count]
-      if level[count] == blankNumber:
-        echo m_vertices[count+1].position.x
-        for d in 0..3:
-          echo d
-          var ground = newSegmentShape(space.staticBody, v(m_vertices[count-1+d].position.x, m_vertices[count-1+d].position.y), v(m_vertices[count+d].position.x, m_vertices[count+d].position.y), 0)
-          ground.friction = 20.0
-          discard space.addShape(ground)
-      count += 1
+        var test: int
+        #if level[drawWidt ==2
+        #echo ""
+    physicsCount += 4
+    #NEW PHYSICS ALGORITHM
+    
+
 
 
 
@@ -145,7 +122,7 @@ var test: Vect = Vect(x:16, y: 16)
 var max = sqrt(level.len.toFloat).toInt
 #echo max
 #echo level.len
-discard createTileMap(12, 7, level, test, 3.0f, 22)
+discard createTileMap(10, 10, level, test, 3.0f, 16)
 
 
 
